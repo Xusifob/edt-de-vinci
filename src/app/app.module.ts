f17a956c4300f3d8e8f91d222cd837e0619e8dd2
xusifob <b.malahieude@free.fr>
2016-11-11 12:34:42 -0800

    Init app

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { IonicApp, IonicModule } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { LoginService } from './services/login.service';
+
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    LoginPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    LoginPage
+  ],
+  providers: [
+      LoginService
+  ]
+})
+export class AppModule {}
