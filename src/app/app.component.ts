0f87caa1a7c7c51d5bac55fc57ae2909ab1f8bc5
xusifob <b.malahieude@free.fr>
2016-11-13 16:21:37 -0800

    update config && add ads

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { LoginPage } from '../pages/login/login';
+declare var adincube : any;
 
 
 @Component({
@@ -15,6 +16,16 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+
+      try {
+        if (adincube) {
+          adincube.setAndroidAppKey('b38ae3a5570e413aa3da');
+          adincube.banner.show(adincube.banner.Size.BANNER_AUTO, adincube.banner.Position.BOTTOM);
+
+
+        }
+      }catch(e){}
+
       StatusBar.styleDefault();
       Splashscreen.hide();
     });
