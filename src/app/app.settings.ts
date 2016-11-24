e39a51ab13c13af60f7c054fbefda30a10e2c929
xusifob <b.malahieude@free.fr>
2016-11-24 14:22:34 -0800

    remove useless home & correct api url

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -1,5 +1,5 @@
 export const SETTINGS = {
-     API_URL : 'http://localhost/annee_4/projects/edt-de-vinci/api/',
+     API_URL : 'http://iim.bastienmalahieude.fr/calendar/edt-de-vinci-api/',
      DEVINCI_CALENDAR_URL : 'https://www.leonard-de-vinci.net/ical_student/',
      EVENT_OFFSET : 6,
      BCIT_CALENDAR_SUFFIX : 'calendar.php?school=bcit',
